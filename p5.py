80b2088a05e8e04ca615e149da50c3bd61a1fcf0
abhi <abhishek.loke2809@gmail.com>
2026-06-30 20:33:10 +0530

    p5.py

--- p5.py
+++ p5.py
@@ -1,30 +1,31 @@
-# PageRank for 3 pages: A, B, C
-# Using formula: PR(A) = (1-d) + d * Σ ( PR(Bi) / C(Bi) )
-
 # PageRank Algorithm
-
 d = 0.85
 links = {
-    'A': ['B', 'C'],
-    'B': ['C'],
-    'C': ['A']
+    "A": ["B", "C"],
+    "B": ["C"],
+    "C": ["A"]
 }
-pages = list(links.keys())
-out = {p: len(links[p]) for p in pages}
-pr = {p: 1.0 for p in pages}
-
+out = {
+    "A": 2,
+    "B": 1,
+    "C": 1
+}
+pr = {
+    "A": 1,
+    "B": 1,
+    "C": 1
+}
 print("Iteration 0")
-for p in pages:
-    print(f"PR({p}) = {pr[p]:.6f}")
+print(pr)
+
 for i in range(1, 3):
-    new_pr = {}
-    for p in pages:
+    new = {}
+    for page in links:
         total = 0
-        for q in pages:
-            if p in links[q]:
-                total += pr[q] / out[q]
-        new_pr[p] = (1-d) + d * total
-    pr = new_pr
-    print(f"\nIteration {i}")
-    for p in pages:
-        print(f"PR({p}) = {pr[p]:.6f}")
+        for link in links:
+            if page in links[link]:
+                total += pr[link] / out[link]
+        new[page] = (1-d) + d * total
+    pr = new
+    print("\nIteration", i)
+    print(pr)
